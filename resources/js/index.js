e9e11df7888c3496f115f900ad91bc53b1a0d715
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2020-11-07 18:53:35 +0530

    contacts.html

--- resources/js/index.js
+++ resources/js/index.js
@@ -166,7 +166,6 @@ var animateButton = function (e) {
     e.preventDefault;
     //reset animation
     e.target.classList.remove("animate");
-
     e.target.classList.add("animate");
     setTimeout(function () {
         e.target.classList.remove("animate");

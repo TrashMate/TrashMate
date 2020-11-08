3fc3f32131fbc9927e52b51b63eb7758807ad8d2
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2020-11-08 16:33:19 +0530

    js to append posts

--- resources/js/index.js
+++ resources/js/index.js
@@ -158,7 +158,7 @@ $(document).ready(function () {
         $(pxlist[i]).paroller();
     }
 
-    $(".trashmate-div").css("top", $(".navbar").height());
+    $(".right-profile-div").css("top", $(".navbar").height());
 });
 
 var a;
@@ -176,4 +176,36 @@ var bubblyButtons = document.getElementsByClassName("bubbly-button");
 
 for (var i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener("click", animateButton, false);
+}
+
+n = 6;
+p = 1;
+lefthtml = $(".left-post-div")[0].outerHTML;
+leftdiv = $(".left-div")[0];
+div = document.createElement("div");
+for (i = 1; i < 34; ++i) {
+    temp = lefthtml.replace("taker1", "taker" + p);
+    div = document.createElement("div");
+    leftdiv.appendChild(div);
+    div.outerHTML = temp;
+    ++p;
+    if (p == 7) {
+        p = 1;
+    }
+}
+
+n = 6;
+p = 1;
+righthtml = $(".right-post-div")[0].outerHTML;
+rightdiv = $(".right-div")[0];
+div = document.createElement("div");
+for (i = 1; i < 34; ++i) {
+    temp = righthtml.replace("taker1", "taker" + p);
+    div = document.createElement("div");
+    rightdiv.appendChild(div);
+    div.outerHTML = temp;
+    ++p;
+    if (p == 7) {
+        p = 1;
+    }
 }
